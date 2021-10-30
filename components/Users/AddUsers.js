f35fe5182c3cade91c6f4d8749dff2da3cab4464
Mohammed Imran <mohdimran97057@gmail.com>
2021-10-30 12:05:52 +0530

    first commit

--- components/Users/AddUsers.js
+++ components/Users/AddUsers.js
@@ -1,63 +1,76 @@
 import React from "react";
 import Card from "../UI/Card";
-import classes from './AddUser.module.css';
+import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import { useState } from "react";
 import ErrorModal from "../UI/ErrorModal";
-
-
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
-    const [error, setError] = useState('')
-    
-    const addUserHandler = (event) => {
-        event.preventDefault();
-        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
-            setError({
-                title: 'InValid input',
-                message: 'Please enter a Valid name and age(non-empty values).'
-            })
-            return;
-        }
-        if(+enteredAge < 1){
-            setError({
-                title: 'InValid Age',
-                message: 'Please enter a Valid  age(age > 0).'
-            })
-            return;
-        }
-       props.onAddUser(enteredUsername, enteredAge)
-        setEnteredUsername('');
-        setEnteredAge('');
-    }
-    const usernameChangeHandler = event => {
-       setEnteredUsername(event.target.value);
-    };
-    const ageChangeHandler = event => {
-        setEnteredAge(event.target.value);
-     };
-    const errorChangeHandler = () => {
-        setError(null);
-    }
-    
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState("");
 
-    return (
-        <div>
-        {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorChangeHandler} />}
-        <Card className={classes.input}>
-         <form onSubmit={addUserHandler}>
-      <label htmlFor="username">UserName</label>
-      <input type="text" id="username" value={enteredUsername} onChange={usernameChangeHandler} ></input>
-      <label htmlFor="age">Age (Years)</label>
-      <input type="number" id="age" value={enteredAge} onChange={ageChangeHandler}></input>
-      <Button type="submit">Add User</Button>
-  </form>
-   
-  </Card>
-  </div>
-    );
-}
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "InValid input",
+        message: "Please enter a Valid name and age(non-empty values).",
+      });
+      return;
+    }
+    if (+enteredAge < 1) {
+      setError({
+        title: "InValid Age",
+        message: "Please enter a Valid  age(age > 0).",
+      });
+      return;
+    }
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+  const errorChangeHandler = () => {
+    setError(null);
+  };
+
+  return (
+   <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorChangeHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">UserName</label>
+          <input
+            type="text"
+            id="username"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          ></input>
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            type="number"
+            id="age"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          ></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </Wrapper>
+  );
+};
 
 export default AddUser;
